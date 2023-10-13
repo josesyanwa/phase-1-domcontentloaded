588e19c32a00f2370ef696c831826cb91bf554c9
Joses Syanwa <josesyanwa@gmail.com>
2023-10-13 13:06:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    var textParagraph = document.getElementById("text");
+  
+    textParagraph.textContent = "This is really cool!";
+  });
+  
